68ab7fa57c952d18f4d3b6566c502fe367e8296c
Lade <oaakintunde@gmail.com>
2025-06-09 16:52:54 -0400

    testing getting metrics 2

--- main.go
+++ main.go
@@ -1,54 +1,18 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"sync/atomic"
-	"time"
 )
 
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
 
-func serverReady(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
 
-}
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	cfg.fileserverHits.Add(1)
-	return next
-}
 
-func (cfg *apiConfig) getNumberOfHits(next http.Handler) atomic.Int32 {
+func main (){
 
-	return cfg.fileserverHits
-
-}
-
-func main() {
-
-	filepathRoot := "."
-	fileServer := http.FileServer(http.Dir(filepathRoot))
-	apiCfg := apiConfig{}
-	const port = "8080"
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", serverReady)
-	mux.Handle("/app/", (http.StripPrefix("/app", fileServer)))
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
-
-	//mux.Handle("assets/logo.png", http.FileServer(http.Dir(imagePath)))
-	server := &http.Server{
-		Addr:        ":" + port,
-		Handler:     mux,
-		ReadTimeout: 10 * time.Second,
+	serveMux := http.serveMux()
+	server := Server{
+		handler : serveMux,
+		Addr : ":8080"
 	}
-
-	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
-	log.Fatal(server.ListenAndServe())
-
 }
