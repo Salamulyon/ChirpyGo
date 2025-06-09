d24f5597a9f94719a63bc707f433583e35402e16
Lade <oaakintunde@gmail.com>
2025-06-09 16:20:25 -0400

    added a ready handler

--- main.go
+++ main.go
@@ -3,22 +3,45 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
+func serverReady(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
+
+}
+
 func main() {
+
+	filepathRoot := "."
+	//imagePath := "/assets/logo.png"
+	//readiness := "/healthz"
 
 	const port = "8080"
 
-	serveMux := &http.ServeMux{}
-
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", serverReady)
+	mux.Handle("/app/", (*apiConfig).middlewareMetrics((http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))))
+	mux.Handle("/app/", (http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	//mux.Handle("assets/logo.png", http.FileServer(http.Dir(imagePath)))
 	server := &http.Server{
 		Addr:        ":" + port,
-		Handler:     serveMux,
+		Handler:     mux,
 		ReadTimeout: 10 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 
 }
