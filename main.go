0124013c50f8398c93d5ff7a5e3b13fd03b3b411
Lade <oaakintunde@gmail.com>
2025-06-17 13:00:41 -0400

    added SQLC

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/Salamulyon/ChirpyGo.git/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Cant access database")
+	}
 
 	filepathRoot := "."
 	fileServer := http.FileServer(http.Dir(filepathRoot))
 	apiCfg := apiConfig{}
 	const port = "8080"
 
+	apiCfg.dbQueries = database.New(db)
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /api/healthz", isServerReady)
--- metrics.go
+++ metrics.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"sync/atomic"
+
+	"github.com/Salamulyon/ChirpyGo.git/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
 }
 
 func isServerReady(w http.ResponseWriter, req *http.Request) {
