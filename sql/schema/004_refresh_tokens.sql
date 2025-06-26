2f622388945ece23b1213c455e7a9f82552c62fb
Lade <oaakintunde@gmail.com>
2025-06-26 13:48:18 -0400

    added jwt authentication

---
+++ sql/schema/004_refresh_tokens.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE refresh_tokens (
+    token string PRIMARY KEY,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    expires_at TIMESTAMP,
+    revoked_at TIMESTAMP,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE refresh_tokens;
